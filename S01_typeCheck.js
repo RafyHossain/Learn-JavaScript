f9188059185e8901f9e96b83c5cfadde2da5fe05
RafyHossain <rafyhossain56@gmail.com>
2025-07-26 22:37:30 +0600

    start javascript learning

---
+++ S01_typeCheck.js
@@ -0,0 +1,9 @@
+var id=2302070;  //variable declaration and initialization
+var name="Rafy Hossain";
+var isStudent=true;
+console.log(id);
+console.log(typeof id); //type of use for variable type check
+console.log(name);
+console.log(typeof name);
+console.log(isStudent);
+console.log(typeof isStudent);
