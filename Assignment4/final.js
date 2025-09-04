d6bbfcc351d571c6f0b12f9924d96759ae68b366
RafyHossain <rafyhossain56@gmail.com>
2025-09-04 16:28:12 +0600

    update

--- Assignment4/final.js
+++ Assignment4/final.js
@@ -11,6 +11,7 @@ function totalFine( fare ) {
    
 }
 
+
 function onlyCharacter(str) {
     if (typeof str !== 'string') {
         return "Invalid";
@@ -69,6 +70,39 @@ if(Array.isArray(arr1)===true && Array.isArray(arr2)===true)
         return false;
 
 
+}
+
+else
+    return "Invalid";
+}
+
+
+
+function  resultReport( marks ) {
+if(Array.isArray(marks)===true){
+    if(marks.length===0)
+        return { finalScore: 0 , pass: 0, fail: 0 };
+    else
+    {
+        let pCount=0;
+        let fCount=0;
+        let total=0;
+       for(let i=0;i<marks.length;i++){
+        total=total+marks[i];
+
+         if(marks[i]>=40){
+            pCount++;
+            
+
+        }
+        else
+            fCount++;
+       }
+       let avg=Math.round(total/marks.length);
+       return { finalScore: avg, pass:pCount,fail: fCount
+    };
+    }
+    
 }
 
 else
